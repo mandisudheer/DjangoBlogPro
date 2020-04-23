f0e8e4b7d6d92f30a49a9821075848fcecad0106
mandisudheer <mandi.sudheer475@gmail.com>
2020-04-23 21:23:07 +0530

    adding requirments.txt

--- env/Src/Myblog/urls.py
+++ env/Src/Myblog/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Personal.views import (
+from Myblog.Personal.views import (
 crete_home_screen,
 )
 urlpatterns = [
